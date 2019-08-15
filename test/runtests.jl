47aa680de34514c5bb981c12c23a0e6cca4347af
aquatiko <rohit970819@gmail.com>
2019-08-15 07:55:40 +0530

    testing dependencies on CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using AstroImageView
+using AstroImages, GtkReactive, Gtk.ShortNames, Graphics, Gtk, Colors, IntervalSets, Cairo
+using AstroImages: render
+
 using Test
 
  @testset "AstroImageView.jl" begin
     # Write your own tests here.
+    @test 2 == 2
+    @test 2 != 4
 end
